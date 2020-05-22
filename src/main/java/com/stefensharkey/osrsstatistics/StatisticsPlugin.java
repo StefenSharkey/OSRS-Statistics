0efe5e4646494193752d0c9d9df243c5248d9ead
Stefen Sharkey <stefensharkey@gmail.com>
2020-05-21 21:26:16 -0400

    Remove internal heatmap generator
    This was removed in favor of a more flexible external heatmap generator.

--- src/main/java/com/stefensharkey/osrsstatistics/StatisticsPlugin.java
+++ src/main/java/com/stefensharkey/osrsstatistics/StatisticsPlugin.java
@@ -26,14 +26,12 @@ package com.stefensharkey.osrsstatistics;
 
 import com.google.inject.Provides;
 import lombok.extern.slf4j.Slf4j;
-import net.runelite.api.ChatMessageType;
 import net.runelite.api.Client;
 import net.runelite.api.GameState;
 import net.runelite.api.NPC;
 import net.runelite.api.Player;
 import net.runelite.api.Skill;
 import net.runelite.api.coords.WorldPoint;
-import net.runelite.api.events.CommandExecuted;
 import net.runelite.api.events.GameStateChanged;
 import net.runelite.api.events.NpcDespawned;
 import net.runelite.api.events.StatChanged;
@@ -204,22 +202,6 @@ public class StatisticsPlugin extends Plugin {
                  "databasepassword",
                  "databasename",
                  "databasetableprefix" -> database.updateConfig(config);
-        }
-    }
-
-    @Subscribe
-    public void onCommandExecuted(CommandExecuted commandExecuted) {
-        switch (commandExecuted.getCommand().toLowerCase()) {
-            case "xpheat":
-                if (HeatMap.IS_GENERATING) {
-                    client.addChatMessage(ChatMessageType.GAMEMESSAGE,
-                            "",
-                            "A heat map is already generating. Please wait for it to complete.",
-                            null);
-                } else {
-                    new Thread(new HeatMap(client, config)).start();
-                }
-                break;
         }
     }
 
